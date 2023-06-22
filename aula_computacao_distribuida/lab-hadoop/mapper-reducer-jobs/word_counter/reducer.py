94bbf8b373dc42e7a4766166125024aabd882617
Wenderson Amaro De Souza <wenderson3souza@gmail.com>
2023-06-22 12:37:08 -0400

    mapper reduccer jobs

---
+++ aula_computacao_distribuida/lab-hadoop/mapper-reducer-jobs/word_counter/reducer.py
@@ -0,0 +1,18 @@
+import sys
+
+currently_word = None
+currently_count = 0
+
+for line in sys.stdin:
+    word, count = line.split('\t')
+    count = int(count)
+    if word == currently_word:
+        currently_count += count
+    else:
+        if currently_word:
+            print('{0};{1}'.format(currently_word, currently_count))
+        currently_word = word
+        currently_count = count
+
+if currently_word:
+    print('{0};{1}'.format(currently_word, currently_count))
