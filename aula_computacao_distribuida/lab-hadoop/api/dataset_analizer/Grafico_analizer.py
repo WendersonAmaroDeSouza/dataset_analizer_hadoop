359d566aeb4ccd2d7cde9eb85a085a0f3ea9b04e
dercides <136863891+dercides@users.noreply.github.com>
2023-06-29 15:22:18 -0400

    Update Grafico_analizer.py

--- aula_computacao_distribuida/lab-hadoop/api/dataset_analizer/Grafico_analizer.py
+++ aula_computacao_distribuida/lab-hadoop/api/dataset_analizer/Grafico_analizer.py
@@ -10,27 +10,38 @@ def api_dados_csv(request):
     dados_csv = DadosCSV.objects.all()
 
 # Datas para o eixo X
+import matplotlib.pyplot as plt
+import datetime
+
+
+
+# Datas para o eixo X
 datas = [
-    datetime.date(2023, 6, 1),
+    datetime.date(2023, 6, 3),
     datetime.date(2023, 6, 5),
     datetime.date(2023, 6, 10),
     datetime.date(2023, 6, 15),
-    datetime.date(2023, 6, 20)
+    datetime.date(2023, 6, 16),
+    datetime.date(2023, 6, 18),
+    datetime.date(2023, 6, 19),
+    datetime.date(2023, 6, 20),
 ]
 
 # Valores para o eixo Y
-valores = [10, 15, 8, 12, 9]
+valores = [10, 15, 8, 12, 9, 6, 16, 18]
 
 # Plotar o gráfico
 plt.plot(datas, valores)
 
 # Configurar o formato das datas no eixo X
-plt.gca().xaxis.set_major_formatter(plt.DateFormatter('%Y-%m-%d'))
-
+plt.xlabel('datas')
+plt.xlabel('valores')
 # Rotacionar as datas para melhor visualização
 plt.gcf().autofmt_xdate()
+plt.title('Grafico de datas')
 
 # Exibir o gráfico
 plt.show()
+ 
  
 return JsonResponse(plt.show())
